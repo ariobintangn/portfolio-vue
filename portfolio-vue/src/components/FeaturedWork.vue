3820df004efa8df49e56d7c41d7f7ff0eca320e6
Ario Bintang <ariobintangn@gmail.com>
2023-06-25 01:16:55 +0700

    justifying featured works

--- portfolio-vue/src/components/FeaturedWork.vue
+++ portfolio-vue/src/components/FeaturedWork.vue
@@ -10,10 +10,12 @@ export default {
             <img src="../assets/Arrow 1.svg" class="mr-3" alt="arrow down">
             <h1 class="text-off-white">FEATURED WORK</h1>
         </div>
-        <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px]"></div>
-        <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px]"></div>
-        <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px]"></div>
-        <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px]"></div>
-        
+
+        <div class="w-full flex flex-col justify-center items-center">
+            <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px] rounded-[36px]"></div>
+            <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px] rounded-[36px]"></div>
+            <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px] rounded-[36px]"></div>
+            <div> <img src="../assets/irwan-claudia.png" alt="1rst project picture" class="h-[600px] w-[366px] object-cover p-[24px] rounded-[36px]"></div>
+        </div>
     </div>
 </template>
